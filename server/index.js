7f702655f23caabbadcde5166ebf9ffe07fb37a8
Duy Dao <ddao@travelers.com>
2023-07-12 16:33:14 -0400

    Added MongoDB to Node.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+//TODO require('dotenv').config();
 
 const app = express();
 // App PORT set with production check
@@ -20,3 +22,31 @@ app.use(express.static('build'));
 
 /* ==================== SET UP ROUTES HERE ========================= */
 app.use('/', (req, res) => res.send('Server is running!'));
+
+
+/* ==================== MongoDB ======================== */
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.touj98i.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
